f704fc150262e405284e4deda9d51d08cdec19f5
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-11-15 21:31:56 +0300

    Added icons

--- app/src/main/java/com/repina/anastasia/momandbaby/Activity/DotsActivity.java
+++ app/src/main/java/com/repina/anastasia/momandbaby/Activity/DotsActivity.java
@@ -19,90 +19,102 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class DotsActivity extends IntroActivity {
-	/**
-	 * Colors to use for the blended background: blue, pink, purple.
-	 */
-	private static final int[] BACKGROUND_COLORS = {0xff304FFE, 0xffcc0066, 0xff9900ff};
+    /**
+     * Colors to use for the blended background: blue, pink, purple.
+     */
+    private static final int[] BACKGROUND_COLORS = {0xff304FFE, 0xffcc0066, 0xff9900ff};
 
-	/**
-	 * Name of the shared preferences which hold a key for preventing the intro screen from
-	 * displaying again once completed.
-	 */
-	public static final String DISPLAY_ONCE_PREFS = "display_only_once_spfile";
+    /**
+     * Name of the shared preferences which hold a key for preventing the intro screen from
+     * displaying again once completed.
+     */
+    public static final String DISPLAY_ONCE_PREFS = "display_only_once_spfile";
 
-	/**
-	 * Key to use in {@code DISPLAY_ONCE_PREFS} to prevent the intro screen from displaying again
-	 * once completed.
-	 */
-	public static final String DISPLAY_ONCE_KEY = "display_only_once_spkey";
+    /**
+     * Key to use in {@code DISPLAY_ONCE_PREFS} to prevent the intro screen from displaying again
+     * once completed.
+     */
+    public static final String DISPLAY_ONCE_KEY = "display_only_once_spkey";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		// Skip to the next Activity if the user has previously completed the introduction
-		/*if (introductionCompletedPreviously()) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Skip to the next Activity if the user has previously completed the introduction
+        /*if (introductionCompletedPreviously()) {
 			final Intent nextActivity = new Intent(this, BabyInfoActivity.class);
 			startActivity(nextActivity);
 			finish();
 		}*/
-		configureTransformer();
-		configureBackground();
-	}
+        configureTransformer();
+        configureBackground();
+    }
 
-	/**
-	 * Generate the pages displayed in this activity.
-	 */
-	@Override
-	protected Collection<Fragment> generatePages(Bundle savedInstanceState) {
-		// This variable holds the pages while they are being created
-		final ArrayList<Fragment> pages = new ArrayList<>();
-		// Create as many pages as there are background colors
-		for (int BACKGROUND_COLOR : BACKGROUND_COLORS) {
-			final ParallaxPage newPage = ParallaxPage.newInstance();
-			int resourceId = R.drawable.common_full_open_on_phone;
-			Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), resourceId);
-			//newPage.setBackImage(mBitmap);
-			newPage.setFrontImage(mBitmap);
-			newPage.setText("dlwldkledfmfmdkfkdfkdfkdkfmdf");
-			pages.add(newPage);
-		}
-		return pages;
-	}
+    /**
+     * Generate the pages displayed in this activity.
+     */
+    @Override
+    protected Collection<Fragment> generatePages(Bundle savedInstanceState) {
+        // This variable holds the pages while they are being created
+        ArrayList<Fragment> pages = new ArrayList<>();
 
-	/**
-	 * Generate the Behaviour of the final button.
-	 */
-	@Override
-	protected IntroButton.Behaviour generateFinalButtonBehaviour() {
+        ParallaxPage newPage = ParallaxPage.newInstance();
+        int resourceId = R.mipmap.mother;
+        Bitmap mBitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        newPage.setFrontImage(mBitmap);
+        newPage.setText("Следите за своим здоровьем");
+        pages.add(newPage);
+
+        newPage = ParallaxPage.newInstance();
+        resourceId = R.mipmap.baby;
+        mBitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        newPage.setFrontImage(mBitmap);
+        newPage.setText("Следите за здоровьем Вашего малыша");
+        pages.add(newPage);
+
+        newPage = ParallaxPage.newInstance();
+        resourceId = R.mipmap.graphic;
+        mBitmap = BitmapFactory.decodeResource(getResources(), resourceId);
+        newPage.setFrontImage(mBitmap);
+        newPage.setText("Наглядная визуальная аналитика");
+        pages.add(newPage);
+
+        return pages;
+    }
+
+    /**
+     * Generate the Behaviour of the final button.
+     */
+    @Override
+    protected IntroButton.Behaviour generateFinalButtonBehaviour() {
 		/* The pending changes to the shared preferences editor will be applied when the
 		 * introduction is successfully completed. By setting a flag in the pending edits and
 		 * checking the status of the flag when the activity starts, the introduction screen can
 		 * be skipped if it has previously been completed.
 		 */
-		final SharedPreferences sp = getSharedPreferences(DISPLAY_ONCE_PREFS, MODE_PRIVATE);
-		final SharedPreferences.Editor pendingEdits = sp.edit().putBoolean(DISPLAY_ONCE_KEY, true);
-		// Define the next activity intent and create the Behaviour to use for the final button
-		final Intent nextActivity = new Intent(this, BabyInfoActivity.class);
-		return new IntroButton.ProgressToNextActivity(nextActivity, pendingEdits);
-	}
+        final SharedPreferences sp = getSharedPreferences(DISPLAY_ONCE_PREFS, MODE_PRIVATE);
+        final SharedPreferences.Editor pendingEdits = sp.edit().putBoolean(DISPLAY_ONCE_KEY, true);
+        // Define the next activity intent and create the Behaviour to use for the final button
+        final Intent nextActivity = new Intent(this, BabyInfoActivity.class);
+        return new IntroButton.ProgressToNextActivity(nextActivity, pendingEdits);
+    }
 
-	/**
-	 * Checks for a shared preference flag indicating that the introduction has been completed
-	 * previously.
-	 */
-	private boolean introductionCompletedPreviously() {
-		final SharedPreferences sp = getSharedPreferences(DISPLAY_ONCE_PREFS, MODE_PRIVATE);
-		return sp.getBoolean(DISPLAY_ONCE_KEY, false);
-	}
+    /**
+     * Checks for a shared preference flag indicating that the introduction has been completed
+     * previously.
+     */
+    private boolean introductionCompletedPreviously() {
+        final SharedPreferences sp = getSharedPreferences(DISPLAY_ONCE_PREFS, MODE_PRIVATE);
+        return sp.getBoolean(DISPLAY_ONCE_KEY, false);
+    }
 
-	private void configureTransformer() {
-		final MultiViewParallaxTransformer transformer = new MultiViewParallaxTransformer();
-		transformer.withParallaxView(R.id.page_fragment_imageHolderFront, 1.2f);
-		setPageTransformer(false, transformer);
-	}
+    private void configureTransformer() {
+        final MultiViewParallaxTransformer transformer = new MultiViewParallaxTransformer();
+        transformer.withParallaxView(R.id.page_fragment_imageHolderFront, 1.2f);
+        setPageTransformer(false, transformer);
+    }
 
-	private void configureBackground() {
-		final BackgroundManager backgroundManager = new ColorBlender(BACKGROUND_COLORS);
-		setBackgroundManager(backgroundManager);
-	}
+    private void configureBackground() {
+        final BackgroundManager backgroundManager = new ColorBlender(BACKGROUND_COLORS);
+        setBackgroundManager(backgroundManager);
+    }
 }
