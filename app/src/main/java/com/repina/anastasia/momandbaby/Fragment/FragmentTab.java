6ea5e3760e887acc6a9f56dc1e5eb3c8c8b94f54
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-11-18 20:11:28 +0300

    Added fragments for switch

--- app/src/main/java/com/repina/anastasia/momandbaby/Fragment/FragmentTab.java
+++ app/src/main/java/com/repina/anastasia/momandbaby/Fragment/FragmentTab.java
@@ -20,10 +20,29 @@ public class FragmentTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_layout, container, false);
-        TextView tv = (TextView) v.findViewById(R.id.text);
-        tv.setText(this.getTag() + " Content");
-        
+        View v;
+        switch (this.getTag()) {
+            case "Analytics": {
+                v = inflater.inflate(R.layout.fragment_analytics, container, false);
+                break;
+            }
+            case "Baby": {
+                v = inflater.inflate(R.layout.fragment_baby, container, false);
+                break;
+            }
+            case "Mom": {
+                v = inflater.inflate(R.layout.fragment_mom, container, false);
+                break;
+            }
+            case "Settings": {
+                v = inflater.inflate(R.layout.fragment_settings, container, false);
+                break;
+            }
+            default: {
+                v = null;
+                break;
+            }
+        }
         return v;
     }
 }
