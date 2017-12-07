bf6e71d1032d4844aa46b2456a100052983bf3fd
Anastasia Repina <anastasiya.repina2012@yandex.ru>
2017-12-07 21:27:55 +0300

    Added icons for new activity

--- app/src/main/java/com/repina/anastasia/momandbaby/Activity/NewActivity.java
+++ app/src/main/java/com/repina/anastasia/momandbaby/Activity/NewActivity.java
@@ -4,26 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.Toast;
 
+import com.repina.anastasia.momandbaby.Classes.CustomGrid;
 import com.repina.anastasia.momandbaby.R;
 
 public class NewActivity extends AppCompatActivity {
+
+    int[] imageId = {
+            R.mipmap.height,
+            R.mipmap.weight,
+            R.mipmap.diapers,
+            R.mipmap.tooth,
+            R.mipmap.vaccination,
+            R.mipmap.illness,
+            R.mipmap.food,
+            R.mipmap.outdoor,
+            R.mipmap.sleep,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
+        final String[] features = getResources().getStringArray(R.array.parameters);
+        CustomGrid adapter = new CustomGrid(NewActivity.this, features, imageId);
+
         GridView grid = (GridView)findViewById(R.id.gridView);
-        ArrayAdapter<?> adapter = ArrayAdapter.createFromResource(this, R.array.parameters, android.R.layout.simple_gallery_item);
         grid.setAdapter(adapter);
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(NewActivity.this, "Position " + position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(NewActivity.this, "You Clicked at " + features[position], Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
